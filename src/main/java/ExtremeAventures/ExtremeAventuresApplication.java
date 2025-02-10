3b7913268efb7f504133fee3d52e995642769b78
Josue Navarro <josue13398@icloud.com>
2025-02-10 17:15:49 -0600

    Inicializando Proyecto ExtremeAventures

---
+++ src/main/java/ExtremeAventures/ExtremeAventuresApplication.java
@@ -0,0 +1,13 @@
+package ExtremeAventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremeAventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremeAventuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExtremeAventures/ExtremeAventuresApplicationTests.java
@@ -0,0 +1,13 @@
+package ExtremeAventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtremeAventuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
